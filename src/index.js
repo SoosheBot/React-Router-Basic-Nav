14166c2b856f9bf0c78e818ad2a4a491941f58cc
sooshe <sooshe@sooshebot.com>
2019-09-17 09:39:11 -0500

    added browser as router to react-router-dom in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Browser as Router} from react-router-dom;
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { domainToASCII } from 'url';
 
 ReactDOM.render(<App />, document.getElementById('root'));
