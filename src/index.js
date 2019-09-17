2bef7e616ffecb2b1a044d96fca3e02dc1f6f5da
sooshe <sooshe@sooshebot.com>
2019-09-17 09:41:17 -0500

    corrected importto browserrouter from react-router-dom in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Browser as Router} from react-router-dom;
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { domainToASCII } from 'url';
 
 ReactDOM.render(<App />, document.getElementById('root'));
